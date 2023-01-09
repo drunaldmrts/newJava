91b69f58659a90dd90556730e81b25f98c75ded6
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-09 10:04:00 +0000

    Employee class

---
+++ src/day35_Encapsulation/Employee.java
@@ -0,0 +1,60 @@
+package day35_Encapsulation;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private char gender;
+    private double salary;
+
+    public Employee(String name, int age, char gender, double salary) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+        setSalary(salary);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isEmpty()){
+            return;
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<16 || age>90){
+            return;
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if(!(gender=='F' || gender=='M')){
+            return;
+        }
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if(salary<=0){
+            return;
+        }
+        this.salary = salary;
+    }
+}

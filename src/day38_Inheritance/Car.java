2aa83fd65936544c51521ce847d858ec0912f1fe
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-24 08:49:07 +0000

    day 38 inheritance metod overriding

---
+++ src/day38_Inheritance/Car.java
@@ -0,0 +1,37 @@
+package day38_Inheritance;
+
+public class Car {
+
+    public String brand, model, color;
+    public int year;
+    public double price;
+    public int miles;
+
+    public Car(String brand, String model, String color, int year, double price, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println(brand+" "+model+" is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand+" "+model+" is driving");
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", miles=" + miles +
+                '}';
+    }
+}

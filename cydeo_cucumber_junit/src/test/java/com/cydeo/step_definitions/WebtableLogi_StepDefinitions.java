a2d3f2ac32702f23850ccd33b4d64418b4019462
Unal Demirtas <drunaldmrts@outlook.com>
2023-03-04 13:47:29 +0000

    Day 17 Web Table Login Test

--- cydeo_cucumber_junit/src/test/java/com/cydeo/step_definitions/WebtableLogi_StepDefinitions.java
+++ cydeo_cucumber_junit/src/test/java/com/cydeo/step_definitions/WebtableLogi_StepDefinitions.java
@@ -1,13 +1,18 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.bouncycastle.asn1.crmf.SinglePubInfo.web;
+
 public class WebtableLogi_StepDefinitions {
     @Given("user is on the login page of web table app")
     public void user_is_on_the_login_page_of_web_table_app() {
-
+        String url = ConfigurationReader.getProperty("web.table.url");
+        Driver.getDriver().get(url);
     }
     @When("user enters username {string}")
     public void user_enters_username(String string) {
